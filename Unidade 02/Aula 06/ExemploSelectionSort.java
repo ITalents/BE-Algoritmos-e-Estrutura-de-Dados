2e6d17dad9585a8397b595b52be968f3cd413e8a
Leoruiz197 <ruizruizleo@hotmail.com>
2023-10-26 17:04:39 -0300

    ajustado com comentarios

--- Unidade 02/Aula 06/ExemploSelectionSort.java
+++ Unidade 02/Aula 06/ExemploSelectionSort.java
@@ -3,13 +3,17 @@ public class ExemploSelectionSort {
     public static void selectionSort(int[] arr){
         int tamanho = arr.length;
 
+        //percorre todo o array
         for(int i = 0; i < tamanho; i++){
             int minIndex = i;
+            //percorre o array novamente para comparar
             for(int j = i + 1; j < tamanho; j++){
+                //compara o elemento do array atual com o selecionado
                 if(arr[j] < arr[minIndex]){
                     minIndex = j;
                 }
             }
+            //swap, troca o elemento pelo encontrado
             int temp = arr[minIndex];
             arr[minIndex] = arr[i];
             arr[i] = temp;
